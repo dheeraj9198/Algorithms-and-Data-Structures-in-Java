bf8df48e40d481faf5762d17a68bc7573e25a698
dheeraj9198 <dhirajsiitk9@gmail.com>
2014-12-22 23:41:51 +0530

    longest increasing subsequence

---
+++ src/LongestIncreasingSubsequence.java
@@ -0,0 +1,97 @@
+/**
+ * Created by dheeraj on 12/22/14.
+ */
+public class LongestIncreasingSubsequence {
+
+    private int[] array;
+    private int length;
+
+    public LongestIncreasingSubsequence(int[] arr) {
+        array = arr;
+        length = array.length;
+    }
+
+
+    public int getLongestIncreasingsubsequenceLength() {
+        int[] maxElementArray = new int[length];
+        int[] lengthArray = new int[length];
+
+        int currentLength;
+
+        for (int x = 0; x < length; x++) {
+            maxElementArray[x] = 0;
+            lengthArray[x] = 0;
+        }
+
+        maxElementArray[0] = array[0];
+        lengthArray[0] = 1;
+        currentLength = 0;
+
+        for (int x = 1; x < length; x++) {
+            int maxIndex = binarySearchMaxSmallest(maxElementArray, array[x], 0, currentLength);
+            if (maxIndex == -1) {
+                //not found
+                currentLength++;
+                maxElementArray[currentLength] = array[x];
+                lengthArray[currentLength] = 1;
+            } else {
+                int lengthTemp = lengthArray[maxIndex] + 1;
+                int lengthCountIndex = binarySearch(lengthArray, lengthTemp, 0, currentLength);
+                if (lengthCountIndex == -1) {
+                    currentLength++;
+                    maxElementArray[currentLength] = array[x];
+                    lengthArray[currentLength] = 1;
+                } else {
+                    maxElementArray[lengthCountIndex] = array[x];
+                }
+            }
+        }
+        return lengthArray[currentLength];
+    }
+
+    private int binarySearchMaxSmallest(int[] array, int data, int start, int end) {
+        if (start == end) {
+            if (array[start] == data) {
+                return start;
+            } else {
+                return -1;
+            }
+        }
+
+        int mid = start + end;
+        mid = mid  /2;
+        if(array[mid] < data && array[mid+1] > data){
+            return mid;
+        }else{
+            if(array[mid] < data){
+                return binarySearchMaxSmallest(array,data,start,mid);
+            }else{
+                return binarySearchMaxSmallest(array,data,mid+1,end);
+            }
+        }
+    }
+
+    private int binarySearch(int[] array, int data, int start, int end) {
+        if (start == end) {
+            if (array[start] == data) {
+                return start;
+            } else {
+                return -1;
+            }
+        }
+
+        int mid = start + end;
+        mid = mid/2;
+        if(array[mid] < data){
+            return binarySearch(array,data,mid+1,end);
+        }else{
+            return binarySearch(array,data,start,mid);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] arr = {2,5,3,7,11,8,10,13,6};
+        LongestIncreasingSubsequence longestIncreasingSubsequence = new LongestIncreasingSubsequence(arr);
+        System.out.println(longestIncreasingSubsequence.getLongestIncreasingsubsequenceLength());
+    }
+}
---
+++ src/WalmartBridge.java
@@ -0,0 +1,51 @@
+/**
+ * Created by dheeraj on 12/21/14.
+ * O(n^3)
+ */
+public class WalmartBridge {
+    private String string1;
+    private String string2;
+
+    public WalmartBridge(String s1,String s2){
+        this.string1 = s1;
+        this.string2 = s2;
+    }
+
+    public int getNumBridges(){
+        int answer = 0;
+        int currentIndex = -1;
+        int length = 0;
+        int tempIndex;
+        for(int x =0;x<string1.length();x++){
+            currentIndex = -1;
+            length = 0;
+            for(int y=x;y<string1.length();y++){
+               tempIndex = findIndex(string1.charAt(y));
+                if(tempIndex == -1){
+                    continue;
+                }
+                if(tempIndex > currentIndex){
+                    length++;
+                    currentIndex = tempIndex;
+                }
+            }
+            answer=Math.max(answer,length);
+        }
+        return   answer;
+    }
+
+    private int findIndex(char c){
+        for(int k =0;k<string2.length();k++){
+            if(string2.charAt(k) == c){
+                return k;
+            }
+        }
+        return -1;
+    }
+
+
+    public static void main(String[] args){
+        WalmartBridge walmartBridge =  new WalmartBridge("ACB","ABC");
+        System.out.println(walmartBridge.getNumBridges());
+    }
+}
