3020512162bc89bbe20a9b1ef910174d97a5dced
dheeraj sachan <dheeraj.sachan@aurusnet.com>
2014-12-31 12:15:37 +0530

    int

---
+++ src/CoreJava/ITest.java
@@ -0,0 +1,10 @@
+package CoreJava;
+
+/**
+ * Created by sujeet on 31/12/14.
+ */
+public interface ITest {
+    //by default all are public
+    int test();
+    int dheeraj(String h);
+}
---
+++ src/CoreJava/TestI.java
@@ -0,0 +1,17 @@
+package CoreJava;
+
+/**
+ * Created by sujeet on 31/12/14.
+ */
+public class TestI implements ITest {
+    // these methods have to be public, since in interface everything is public so visibility cannot be reduced
+    @Override
+    public int test() {
+        return 0;
+    }
+
+    @Override
+    public int dheeraj(String h) {
+        return Integer.parseInt(h);
+    }
+}
