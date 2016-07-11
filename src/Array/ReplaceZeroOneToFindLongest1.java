38a97ae69f069fadc4e5d100157ed7c11adc8a4f
sachan <dhirajsiitk9@gmail.com>
2016-07-11 14:49:22 +0530

    replace one 0 to get longest ones

--- src/Array/ReplaceZeroOneToFindLongest1.java
+++ src/Array/ReplaceZeroOneToFindLongest1.java
@@ -5,12 +5,51 @@ package Array;
  */
 public class ReplaceZeroOneToFindLongest1 {
     public static void main(String[] strings) {
-        int[] array = new int[]{1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1};
+/*
+        int[] array = new int[]{1, 0, 1, 1, 0,1, 0, 1, 1, 1, 0, 0};
+*/
+        int[] array = new int[]{1, 1, 1, 1, 0,1, 1, 1, 1, 1, 1, 1};
 
-        int start, rep, length;
+        int index = -1;
+        int maxlen = 0;
+
+        int prevp = -1;
+        Integer prevprev0 = null;
 
         for (int x = 0; x < array.length; x++) {
+            if (array[x] == 0) {
+                if (prevp == -1) {
+                        prevp = x;
+                    index = x;
+                }else{
+                    if(prevprev0 == null){
+                        //only one zero found till now
+                        if(x > maxlen){
+                            index = prevp;
+                            maxlen = x;
+                        }
+                    }else if(x-prevprev0-1 > maxlen){
+                        //prev prev 0 excluded
+                        index = prevp;
+                        maxlen = x-prevprev0-1;
+                    }
+                    prevprev0 = prevp;
+                    prevp = x;
+                }
 
+            }
+        }
+
+        //check if last is 1
+        if(array[array.length-1] == 1 && prevprev0 != null){
+            if((array.length-1)-prevprev0 > maxlen){
+                index = prevp;
+            }
+        }
+        if(prevprev0 == null && prevp == -1){
+            System.out.print(-1);
+        }else{
+            System.out.println(index);
         }
 
     }
